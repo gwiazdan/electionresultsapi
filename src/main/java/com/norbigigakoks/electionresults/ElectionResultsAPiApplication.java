400bcfb0be649cf5f8d90ac9348dfd2062fc69c7
norbigigakoks <norbeegsoft@gmail.com>
2024-10-12 00:40:18 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/norbigigakoks/electionresults/ElectionResultsAPiApplication.java
@@ -0,0 +1,13 @@
+package com.norbigigakoks.electionresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionResultsAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionResultsAPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/norbigigakoks/electionresults/ElectionResultsAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbigigakoks.electionresults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionResultsAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
