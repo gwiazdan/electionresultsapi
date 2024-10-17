aa3aef38ca91637edc57710a992de70833897894
norbigigakoks <norbeegsoft@gmail.com>
2024-10-17 18:24:01 +0200

    Dodano Senat i Wyniki do Senatu

---
+++ src/main/java/com/norbigigakoks/electionresults/TerritoriesRepository.java
@@ -0,0 +1,10 @@
+package com.norbigigakoks.electionresults;
+
+import com.norbigigakoks.electionresults.units.Territory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TerritoriesRepository extends JpaRepository<Territory, Long> {
+    public List<Territory> findAll();
+}
